c60409c3d84d7bfaf129cbbf31dd11b356bcc787
hasitha <hasithamadusanka90@gmail.com>
2019-04-06 17:40:14 +0530

    single file templates

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VeeValidate from 'vee-validate';
 
-Vue.use(VeeValidate);
+import Home from './Home.vue'
+
 Vue.config.productionTip = false
 
+Vue.component('app-server-status',Home)
+
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+});
